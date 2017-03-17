4dafdd1c7da591ec2975430517ec10e26c1ca053
Baptiste VEUILLEZ--MAINARD <baptiste.veuillez--mainard@telecom-bretagne.eu>
2017-03-17 16:50:30 +0100

    typer now checks if the methods are called with the right argument types, and Call expressions are typed as the return type of the function

--- Typing/Typing.ml
+++ Typing/Typing.ml
@@ -4,7 +4,20 @@ open Type
 exception NonBooleanCondition;;
 exception BadTypeDefaultValue;;
 exception IncoherentTypes;;
+exception UnknownMethod;;
 
+type env = {methods: AST.astmethod list ; attributes: AST.astattribute list};;
+
+let build_env env ac = 
+    let rec aux methods =
+        match methods with
+        | [] -> []
+        | {AST.mmodifiers;mname;mreturntype;margstype;mthrows;mbody}::l ->
+                print_string ("adding "^mname^" to env\n");
+                {AST.mmodifiers;mname;mreturntype;margstype;mthrows;mbody=[]}::(aux l)
+    in match env,ac with {methods;attributes},{AST.cparent; AST.cattributes; AST.cinits; AST.cconsts; cmethods; ctypes; cloc} ->
+        {methods=(aux cmethods)@methods;attributes};;
+        
 (* How does this make any sense ? Typing a type ? Was used for verbosity purpose, will be removed before the typer is over. *)
 let rec process_t t =
     match t with
@@ -14,7 +27,7 @@ let rec process_t t =
     | Ref r -> print_string ("ref "^(Type.stringOf t)^"\n");;
 
 (*the big stuff, adding the type information to the expressions*)
-let rec process_expression exp:AST.expression = 
+let rec process_expression env exp:AST.expression = 
     match exp with {AST.edesc; AST.etype} ->
         match etype with
         | Some etype -> print_string ("Expression of type: "^(stringOf etype)^"\n"); exp;
@@ -23,12 +36,53 @@ let rec process_expression exp:AST.expression =
         match edesc with
             | New(Some name,id,params) -> print_string ("new "^name^": "^(AST.string_of_expression_desc edesc)); exp;
             | NewArray(_,_,_) -> print_string "newarray todo\n"; exp;
-            | Call(o,id,args) -> print_string ("call "^id^" todo\n"); exp;
-            | Attr(expr,id) -> process_expression expr; print_string ("."^id^"\n"); exp;
+            | Call(o,id,args) ->
+                    print_string ("calling "^id^"\n");
+                    print_string "evaluating args\n";
+                    let args = List.map (process_expression env) args in
+                    print_string ("looking up "^id^" in env.methods\n");
+                    let rec aux methods = match methods with
+                    | [] -> print_string "I don't think this method is in env\n"; None
+                    | m::methods ->
+                            print_string ("considering "^m.AST.mname^"\n");
+                            if m.AST.mname=id
+                            then 
+                            (
+                                print_string "that could be it ! Checking the type of the arguments\n";
+                                if (List.length args) = (List.length m.margstype)
+                                then
+                                (
+                                    print_string "right number of args\n";
+                                    let rec aux2 args argstype =
+                                        match args,argstype with
+                                        | [],[] -> true;
+                                        | {AST.edesc;etype=Some a}::args,t::argstype ->
+                                                if a=t.AST.ptype
+                                                then aux2 args argstype
+                                                else (print_string "wrong argtype\n"; false);
+                                    in
+                                    if (aux2 args m.margstype)
+                                    then (print_string "ok this is the right method\n"; Some m;)
+                                    else aux methods;
+                                )
+                                else
+                                (
+                                    print_string "wrong number of args\n";
+                                    aux methods;
+                                )
+                            )
+                            else (print_string "that's not it\n"; aux methods);
+                    in 
+                    (
+                        match aux env.methods with
+                        | None -> raise UnknownMethod;
+                        | Some m -> {edesc;etype=Some m.mreturntype};
+                    );
 
-            |  CondOp(condition, iftrue, iffalse) -> 
+            | Attr(expr,id) -> print_string "attribute "; print_string (id^" of\n"); process_expression env expr;
+            | CondOp(condition, iftrue, iffalse) -> 
                     print_string ("condop\n");
-                    let r =  AST.CondOp((process_expression condition), (process_expression iftrue), (process_expression iffalse)) in
+                    let r =  AST.CondOp((process_expression env condition), (process_expression env iftrue), (process_expression env iffalse)) in
                     (
                     match r with CondOp({edesc=_;etype=etypecond}, {edesc=_;etype=etypetrue}, {edesc=_;etype=etypefalse}) ->
                         if etypecond=Some (Primitive Boolean)
@@ -59,7 +113,7 @@ let rec process_expression exp:AST.expression =
  * turns out, I was mistaking If for CondOp, but it does not solve the If problem if it arrises later
  * also I don't know what the If is supposed to be as an expression, I wonder if it is not actually used, was replaced by condop and forgotten ... poor If expression
  * for the record, the error was on the following :
- * AST.If((process_expression condition), (process_expression iftrue), (process_expression iffalse))
+ * AST.If((process_expression env condition), (process_expression env iftrue), (process_expression env iffalse))
  *)
 
             | If(_,_,_) -> exp;
@@ -77,7 +131,7 @@ let rec process_expression exp:AST.expression =
             );
             | Name name -> print_string ("name: "^name^"\n"); exp;
             | Post(exp,op) -> 
-                    let r = process_expression exp in
+                    let r = process_expression env exp in
                     (
                     match r with
                     | {edesc;etype=Some t} ->
@@ -90,12 +144,12 @@ let rec process_expression exp:AST.expression =
                             {AST.edesc=Post(r,op);etype=None};
                     );
             | Pre(op,exp) -> 
-                    let r = process_expression exp in
+                    let r = process_expression env exp in
                     (
                     match r with {edesc;etype} -> {AST.edesc=Pre(op,r);etype}; (* needs more verbosity and verifications as in previous case ... *)
                     );
             | Op(exp1,op,exp2) -> 
-                    let r1 = process_expression exp1 and r2 = process_expression exp2 in
+                    let r1 = process_expression env exp1 and r2 = process_expression env exp2 in
                     (
                         match r1,r2 with 
                         | {edesc=_;etype=Some t1},{edesc=_;etype=Some t2} ->
@@ -110,7 +164,7 @@ let rec process_expression exp:AST.expression =
                     );
             | Cast (t,exp) ->
                     (
-                    match (process_expression exp) with {edesc;etype = Some etype} ->
+                    match (process_expression env exp) with {edesc;etype = Some etype} ->
                         print_string ("casting from "^(stringOf etype)^" to "^(stringOf t)^" and I don't care if it makes any sense\n");
                         {edesc;etype=Some t};
                     );
@@ -118,22 +172,22 @@ let rec process_expression exp:AST.expression =
             | Type t -> print_string ("This expression is a type, I don't know what is the type of this type: "^(Type.stringOf t)); exp;
             (* not sure about the following two either ...*)
             | ClassOf t -> print_string ("ClassOf: "^(Type.stringOf t)); exp;
-            | Instanceof (exp,t) -> print_string ("InstanceOf: "^(Type.stringOf t)); process_expression exp;
+            | Instanceof (exp,t) -> print_string ("InstanceOf: "^(Type.stringOf t)); process_expression env exp;
             | _ -> print_string "expression todo\n"; print_string (AST.string_of_expression_desc edesc); print_string "\nend todo\n";  exp;
         )
         ;;
 
-let rec process_statement statement =
+let rec process_statement env statement =
     match statement with
     | AST.Block statements ->
             print_string "start block\n";
-            let r = AST.Block(List.map process_statement statements) in
+            let r = AST.Block(List.map (process_statement env) statements) in
             print_string "end block\n";
             r;
     | Nop -> print_string "nope\n"; statement;
     | While(exp,statement) -> 
             print_string "while\n";
-            let r = AST.While(process_expression exp, process_statement statement) in
+            let r = AST.While(process_expression env exp, process_statement env statement) in
             (
                 match r with AST.While({edesc;etype},_) ->
                     if etype=Some (Primitive Boolean)
@@ -143,12 +197,12 @@ let rec process_statement statement =
             ); r;
     | For(truc1,truc2,truc3,statement) ->
             print_string "for need some more work\n";
-            let r = AST.For(truc1,truc2,truc3, process_statement statement) in
+            let r = AST.For(truc1,truc2,truc3, process_statement env statement) in
             print_string "end for\n";
             r;
     | If(cond,iftrue,iffalse) -> 
             print_string "if\n";
-            let r = AST.If(process_expression cond, process_statement iftrue, (match iffalse with None -> None | Some statement -> Some (process_statement statement))) in
+            let r = AST.If(process_expression env cond, process_statement env iftrue, (match iffalse with None -> None | Some statement -> Some (process_statement env statement))) in
             (
                 match r with 
                     | AST.If({edesc;etype},_,_) ->
@@ -162,13 +216,13 @@ let rec process_statement statement =
             (
                 match exp with 
                 | None -> print_string "return void\n"; AST.Return None;
-                | Some exp -> print_string "return\n"; let r = AST.Return( Some (process_expression exp)) in print_string "end return\n"; r;
+                | Some exp -> print_string "return\n"; let r = AST.Return( Some (process_expression env exp)) in print_string "end return\n"; r;
             );
-    | AST.Expr expr -> Expr(process_expression expr);
+    | AST.Expr expr -> Expr(process_expression env expr);
     | _ -> print_string "statement todo\n"; statement;
 ;;
 
-let rec process_astattributes astattributes =
+let rec process_astattributes env astattributes =
     match astattributes with 
     | [] -> [];
     | {AST.amodifiers;aname;atype;adefault}::l ->
@@ -179,7 +233,7 @@ let rec process_astattributes astattributes =
         (
             match adefault with
             | None -> print_string "No default\n"; None;
-            | Some exp -> Some (process_expression exp);
+            | Some exp -> Some (process_expression env exp);
         ) in
         ( 
             match def with Some {AST.edesc; AST.etype} ->
@@ -192,7 +246,7 @@ let rec process_astattributes astattributes =
                         then print_string "init value type correct\n"
                         else (print_string ("wrong init value type ! "^(stringOf atype)^" vs "^(stringOf t)^"\n"); raise BadTypeDefaultValue;)
         );
-        {AST.amodifiers;aname;atype;adefault=def}::(process_astattributes l);;
+        {AST.amodifiers;aname;atype;adefault=def}::(process_astattributes env l);;
         
 let rec process_arguments arguments =
     match arguments with
@@ -201,7 +255,7 @@ let rec process_arguments arguments =
         process_t ptype;
         process_arguments l;;
 
-let rec process_astmethods astmethods =
+let rec process_astmethods env astmethods =
     match astmethods with
     | [] -> [];
     | {AST.mmodifiers;mname;mreturntype;margstype;mthrows;mbody}::l ->
@@ -210,18 +264,20 @@ let rec process_astmethods astmethods =
         print_string "return type: "; process_t mreturntype;
         print_string "args: "; process_arguments margstype;
         print_string "body: \n";
-        let b = (List.map process_statement mbody);
+        let b = (List.map (process_statement env) mbody);
         in
-        {AST.mmodifiers;mname;mreturntype;margstype;mthrows;mbody=b}::(process_astmethods l);;
+        {AST.mmodifiers;mname;mreturntype;margstype;mthrows;mbody=b}::(process_astmethods env l);;
 
-let process_astclass ac =
+let process_astclass env ac =
     match ac with {AST.cparent; AST.cattributes; AST.cinits; AST.cconsts; cmethods; ctypes; cloc} ->
+        let env = build_env env ac in 
         print_string "\nattributes\n";
-        let tribute = process_astattributes cattributes in
+        let tribute = process_astattributes env cattributes in
         print_string "\nmethods\n";
-        {AST.cparent;cattributes=tribute; cinits; cconsts; cmethods=(process_astmethods cmethods); ctypes; cloc};;
+        {AST.cparent;cattributes=tribute; cinits; cconsts; cmethods=(process_astmethods env cmethods); ctypes; cloc};;
 
 let rec process_type_list l =
+    let env={methods=[];attributes=[]} in
     match l with
     | [] -> []
     | {AST.modifiers; AST.id; AST.info}::l ->
@@ -230,7 +286,7 @@ let rec process_type_list l =
         let i = 
         (
             match info with
-                | Class astclass -> AST.Class (process_astclass astclass);
+                | Class astclass -> AST.Class (process_astclass env astclass);
                 | Inter -> print_string "Inter\n"; Inter;
         ) in
         {AST.modifiers;id;info=i}::(process_type_list l);;
